60257e8d9da649518004b282447bd6d125c4ace7
BiBiLog <dzugaev2002@gmail.com>
2022-03-21 09:33:57 +0300

    Ses

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Controls;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1_1()
+        {
+            int a = 3;
+            int b = 5;
+            int c = 6;
+            int rez = 90;
+
+            Page p = new Page();
+            int actual = p.TestMethod1_1(a, b, c);
+            Assert.AreEqual(rez, actual);
+        }
+    }
+}
---
+++ WpfApp2/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int rez;
+            var a = Convert.ToInt32(Text1.Text);
+            var b = Convert.ToInt32(Text2.Text);
+            var c = Convert.ToInt32(Text3.Text);
+            rez = a * b * c;
+            Text4.Text = rez.ToString();
+        }
+    }
+}
